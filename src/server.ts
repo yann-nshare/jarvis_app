d73b059e9e609d6096fbbe6c6aa35377652e8779
Vincent Zhong <vzhong@localhost.localdomain>
2021-02-20 20:42:37 +0100

    [FIX] Non compiling

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import DB from './appdata';
 import { env } from './const_env';
-// import create_client from 'models/client';
+import { create_client } from './models/client';
+import { create } from 'domain';
 
+//export var db;
 // my env is at the root of jarvis_app -> ".env"
 require('dotenv').config({ path: __dirname+'/.env' });
 
@@ -21,25 +23,25 @@ app.use(
 
 //Start connection
 app.listen(8080, async() => {
-  await DB()
+  var db = await DB()
   console.log('listening on port 8080!');
 });
 
 // //Register for client
-// app.post('/register/client', async (req, res) => {
-//   try {
-//     const{username, password, email} = req.body
-//     console.log(username, password, email)
-//     //Create client
-//     create_client()
-//     console.log(req.body)
-//     res.send("client Registered")
-//     res.status(200)
-//   } catch (error) {
-//     res.send('body error')
-//     res.status(500)
-//   }
-// });
+app.post('/register/client', async (req, res) => {
+  try {
+    const{username, password, email} = req.body
+    console.log(username, password, email)
+    //Create client
+    create_client(username, password, email);
+    console.log(req.body)
+    res.send("client Registered")
+    res.status(200)
+  } catch (error) {
+    res.send('body error')
+    res.status(500)
+  }
+});
 
 // //Register for pro
 // app.post('/register/pro', async (req, res) => {
